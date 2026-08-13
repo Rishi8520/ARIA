895915699ebf3d8255a30c3ba21c00586b810827
Rishi8520 <rishiraj.mitblr2022@learner.manipal.edu>
2026-08-13 19:20:18 +0530

    aria_v2/src/imu_thread.h: This is the corresponding .h file for imu_thread_entry.c.

---
+++ CODE_v2/aria_v2/src/imu_thread.h
@@ -0,0 +1,14 @@
+#ifndef IMU_THREAD_H_
+#define IMU_THREAD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void imu_thread_entry(void *pvParameters);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* IMU_THREAD_H_ */
